84d03160ab801f8c58ef6f977196fffa87bcddc5
tiffihaag <tiffihaag@gmail.com>
2016-08-03 08:00:38 -0400

    Throw it up

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+
+	$('#subButton').on('click', function() {
+        var politician = $('#politician').val();
+        console.log(politician);
+
+        $(".imgPlace").empty();
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + politician + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({url: queryURL, method: 'GET'})
+        .done(function(response) {
+        	for (var i = 0; i < 10; i++) {
+	        	var imageUrl = response.data[i].images.downsized_large.url;
+	        	console.log(imageUrl);
+	        	var imgHtml = "<img src = '" + imageUrl + "' height='200' width='200'>";
+	        	$(imgHtml).appendTo(".imgPlace");
+        	}
+
+        	var newButton = "<button class='polButton2' data-person='" + politician + "'>" + politician + "</button>";
+	        $(newButton).appendTo("#buttonList");
+
+	        $('.polButton2').on('click', function() {
+		        var politician = $(this).attr("data-person");
+		        console.log(politician);
+		        getPics(politician);
+
+			}); //onclick function
+        });     
+	}); //onclick function
+
+	$('.polButton').on('click', function() {
+        var politician = $(this).attr("data-person");
+        console.log(politician);
+        getPics(politician);
+
+	}); //onclick function
+
+	function getPics(politician) {
+		$(".imgPlace").empty();
+			console.log(politician);
+
+	 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + politician + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+	        $.ajax({url: queryURL, method: 'GET'})
+	        .done(function(response) {
+	        	for (var i = 0; i < 10; i++) {
+		        	var imageUrl = response.data[i].images.downsized_large.url;
+		        	//console.log(imageUrl);
+
+		        	var imgHtml = "<img src = '" + imageUrl + "' height='200' width='200'>";
+		        	$(imgHtml).appendTo(".imgPlace");
+	        	}
+	        });
+		}
+}); //docready function
